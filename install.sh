f858553a06712456ef034d03ad5239ecf29b62ba
Clayton Auld <clayauld@gmail.com>
2021-10-08 13:11:21 -0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 mkdir ../../www/input-timetable
 cd ../../www/input-timetable
-ln -s  ../../custom_components/hass-input-timetable/timetable-card.js
+ln -s  ../../custom_components/hass-input-timetable/timetable-card.js ./
